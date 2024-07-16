8cc389a9920658167c008a8b09b4cafca05417e1
Scott MacDonald <kenobi@Scotts-MacBook-Pro.local>
2024-07-15 19:49:29 -0700

    Added difficulty select

--- number_guess.py
+++ number_guess.py
@@ -1,4 +1,19 @@
-from modules.art import logo
+# from modules.art import logo
+from random import randint
 
+answer = randint(1, 100)
 
-print(logo)
+is_playing = False
+EASY_LIVES = 5
+DIFFICULT_LIFE = 10
+
+def select_difficulty():
+    difficulty = input("Please select difficulty. Type 'Hard' or 'Easy'\n").lower()
+    if difficulty == "Hard":
+        DIFFICULT_LIFE
+        print(f"You have {DIFFICULT_LIFE} attempts.")
+    else:
+        EASY_LIVES
+        print(f"You have {EASY_LIVES} attempts.")
+
+select_difficulty()
