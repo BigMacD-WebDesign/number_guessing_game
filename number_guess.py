945837843317b9124cef380afd33055aea6a6832
Scott MacDonald <kenobi@Scotts-MacBook-Pro.local>
2024-07-15 20:02:32 -0700

    Added remaining logic to play game

--- number_guess.py
+++ number_guess.py
@@ -1,19 +1,42 @@
-# from modules.art import logo
-from random import randint
+from modules.art import logo
+import random
 
-answer = randint(1, 100)
+print(logo)
+print("Welcome to GUESS THAT NUMBER!")
+
+def computer_number():
+    """Computer creates a random number between 1 and 100"""
+    choice = random.randint(1, 100)
+    return choice
+
+computers_number = computer_number()
+print(f"Psst, the correct answer is {computers_number}")
 
 is_playing = False
-EASY_LIVES = 5
-DIFFICULT_LIFE = 10
 
-def select_difficulty():
-    difficulty = input("Please select difficulty. Type 'Hard' or 'Easy'\n").lower()
-    if difficulty == "Hard":
-        DIFFICULT_LIFE
-        print(f"You have {DIFFICULT_LIFE} attempts.")
-    else:
-        EASY_LIVES
-        print(f"You have {EASY_LIVES} attempts.")
+difficulty = input("Please select difficulty. Type 'Hard' or 'Easy'\n").lower() 
+if difficulty == "hard":
+    player_lives = 5
+    print(f"You have {player_lives} lives.")
+elif difficulty == "easy":
+    player_lives = 10
+    print(f"You have {player_lives} lives.")
 
-select_difficulty()
+
+while not is_playing:
+    player_guess = int(input("Please guess a number between 1 and 100: "))
+    if player_guess > 100:
+        print("Please select a number between 1 - 100.")   
+    if player_guess >  computers_number:
+       player_lives -= 1
+       print(f"Too high. You have {player_lives} lives remaining.")
+    elif player_guess < computers_number:
+       player_lives -= 1
+       print(f"Too low. You have {player_lives} lives remaining.")
+       
+    if player_lives == 0:
+        print(f"Sorry you lose. You ran out of lives. The number was {computers_number}")
+        is_playing = True   
+    elif player_guess == computers_number:
+        print(f"You win. The number is {computers_number}")
+        is_playing = True       
